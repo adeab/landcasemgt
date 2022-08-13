7fd051bcd26c91504def54af6b0243d00d9aa85b
adeab <adeab@live.com>
2022-08-13 22:27:20 +0600

    updated design and user type column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>উপজেলা ভূমি অফিস, গৌরনদী, বরিশাল</title>
-    <link rel="icon" type="image/png" href="{{asset('images/bd-gov-logo.png')}}">
+    <link rel="icon" type="image/png" href="{{ asset('images/bd-gov-logo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Font Awesome Icons -->
@@ -16,81 +16,70 @@
 </head>
 
 <body class="mainbody">
-    <div class="d-flex justify-content-center">
-        <div class="card homepage-card">
-            {{-- <div class="card-header homepage-card-header">
-                <div class="row col-12">
-                    <div class="col-md-6 left-aligned-content">
-                        ভূমি মন্ত্রণালয়, গণপ্রজাতন্ত্রী বাংলাদেশ সরকার
-                    </div>
-                    <div class="col-md-6 right-aligned-content">
-                        @livewire('components.currentday')
-                    </div>
-                </div>
-            </div> --}}
-            <div class="card-header homepage-card-header2">
-                <div class="row col-12">
-                    {{-- <div class="col-md-6"> --}}
+    <div class="layer">
+        <div class="d-flex justify-content-center">
+            <div class="card homepage-card">
+                <div class="card-header homepage-card-header2">
+                    <div class="row col-12">
+                        {{-- <div class="col-md-6"> --}}
                         <div class="d-flex flex-row align-items-center justify-content-center">
-                            <x-land-logo class="logo-home"/>
+                            <x-land-logo class="logo-home" />
                         </div>
                         <div class="d-flex flex-row align-items-center justify-content-center">
-                            <x-project-title class="title-home"/>
-                        </div>
-                    {{-- </div> --}}
-                    {{-- <div class="col-md-6 right-aligned-content">
-                        <div class="d-flex flex-row-reverse align-items-center">
-                            <x-project-logo class="logo-home"/>
-                            <x-credit style="padding: 1rem;"/>
-                        </div>
-                    </div> --}}
-
-
-                </div>
-
-            </div>
-            <div class="nav-section">
-                <x-navbar-home/>
-            </div>
-            <div class="card-body homepage-card-body text-center">
-                <div class="d-flex justify-content-center">
-                    {{ $slot }}
-                </div>
-            </div>
-            <div class="card-footer text-center upper-footer">
-                <div class="row upper-footer-row">
-                    <div class="col-md-4 footer-column text-center">
-                        <div class="fo-title"><h2>গুরুত্বপূর্ণ লিঙ্ক</h2></div>
-                        <div class="foo-content text-center">
-                            <a target="_blank" href="http://acl.gournadi.barisal.gov.bd/">acl.gournadi.barisal.gov.bd</a>
+                            <x-project-title class="title-home" />
                         </div>
                     </div>
-                    <div class="col-md-4 footer-column text-center">
-                        <div class="fo-title"><h2>সামাজিক যোগাযোগ </h2></div>
 
-                        <div class="foo-content text-center">
-                            <a class="social-icons facebook" target="_blank" href="http://www.facebook.com/minland.gov.bd"><i class="fa fa-facebook"></i></a>
-                            <a class="social-icons youtube" target="_blank" href="https://www.youtube.com/channel/UCBlCPkpMK00tcPcdR-DTxrw"><i class="fa fa-youtube"></i></a>
-                        </div>
-                    </div>
-                    {{-- <div class="col-md-3 footer-column text-center">
-                        <div class="fo-title"><h2>পরিকল্পনা ও বাস্তবায়নে  </h2></div>
-
-                        <div class="foo-content text-center">
-                            <a href="https://minland.gov.bd/" target="_blank"><img src="https://land.gov.bd/wp-content/themes/land-portal/images/icons/land.png" alt=""></a>
-                            <a href="https://a2i.gov.bd/" target="_blank"><img src="https://land.gov.bd/wp-content/themes/land-portal/images/icons/a2i.png" alt=""></a>
-                        </div>
-                    </div> --}}
-                    <div class="col-md-4 footer-column text-center">
-                        <div class="fo-title"><h2>কারিগরি সহায়তায়</h2></div>
-                        <div class="foo-content text-center">
-                            <a href="https://taqitizar.com/" target="_blank"><img class="img-fluid sponsor-img" src="{{asset('images/taqi-logo.png')}}" alt=""></a>
+                </div>
+                <div class="nav-section">
+                    <x-navbar-home />
+                </div>
+                <div class="card-body homepage-card-body text-center">
+                    <div class="layer2">
+                        <div class="d-flex justify-content-center">
+                            {{ $slot }}
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="card-footer text-center lower-footer">
-               <x-copyright/>
+                <div class="card-footer text-center upper-footer">
+                    <div class="row upper-footer-row">
+                        <div class="col-md-4 footer-column text-center">
+                            <div class="fo-title">
+                                <h2>গুরুত্বপূর্ণ লিঙ্ক</h2>
+                            </div>
+                            <div class="foo-content text-center">
+                                <a target="_blank" class="link-text"
+                                    href="http://acl.gournadi.barisal.gov.bd/">acl.gournadi.barisal.gov.bd</a>
+                            </div>
+                        </div>
+                        <div class="col-md-4 footer-column text-center">
+                            <div class="fo-title">
+                                <h2>সামাজিক যোগাযোগ </h2>
+                            </div>
+
+                            <div class="foo-content text-center">
+                                <a class="social-icons facebook" target="_blank"
+                                    href="http://www.facebook.com/minland.gov.bd"><i class="fa fa-facebook"></i></a>
+                                <a class="social-icons youtube" target="_blank"
+                                    href="https://www.youtube.com/channel/UCBlCPkpMK00tcPcdR-DTxrw"><i
+                                        class="fa fa-youtube"></i></a>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 footer-column text-center">
+                            <div class="fo-title">
+                                <h2>কারিগরি সহায়তায়</h2>
+                            </div>
+                            <div class="foo-content text-center">
+                                <a href="https://taqitizar.com/" target="_blank"><img class="img-fluid sponsor-img"
+                                        src="{{ asset('images/taqi-logo.png') }}" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-center lower-footer">
+                    <x-copyright />
+                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/pages/welcome.blade.php
+++ resources/views/livewire/pages/welcome.blade.php
@@ -1,30 +1,22 @@
-
-
-
-
-
 <div class="container center-element home-card">
     <div class="card-body">
         <div class="row">
-        <div class="call-section">
-            <x-project-logo class="logo-home"/>
-            <x-credit/>
+            <div class="call-section">
+                <x-project-logo class="logo-home" />
+                <x-credit />
+            </div>
         </div>
-
-    {{-- <a target="_blank" href="https://hotline.land.gov.bd/" class="call-section"><img src="{{asset('images/call-center.png')}}"></a> --}}
-</div>
-    <div class="row">
-        <button class="home-button citizen-btn" wire:click="gotocasesearch">
-            <i class="fas fa-users"></i>
-            Citizen Corner
-        </button>
-    </div>
-
-    <div class="row">
-        <button class="home-button ofc-btn" wire:click="gotologin">
-            <i class="fas fa-user-lock"></i>
-            Office Corner
-        </button>
-    </div>
+        <div class="row">
+            <button class="home-button citizen-btn" wire:click="gotocasesearch">
+                <i class="fas fa-users"></i>
+                Citizen Corner
+            </button>
+        </div>
+        <div class="row">
+            <button class="home-button ofc-btn" wire:click="gotologin">
+                <i class="fas fa-user-lock"></i>
+                Office Corner
+            </button>
+        </div>
     </div>
 </div>
